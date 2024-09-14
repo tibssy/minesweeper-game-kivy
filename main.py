5d3c2fb44d7084f34f5e15b4872ed83e007b3dbe
tibssy <norbert.tiborcz@gmail.com>
2024-09-14 20:17:00 +0100

    Add main screen with content

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.factory import Factory
 
 
 Builder.load_file('layout.kv')
 
 
-class MainLayout(BoxLayout):
-    pass
+if Window._is_desktop:
+    Window.size = 500, 1000
+
+
+class MainLayout(ScreenManager):
+    def toggle_screen(self):
+        if self.current == 'main_screen':
+            self.transition.mode = 'pop'
+            self.transition.direction = 'up'
+            self.current = 'game_screen'
+
+        else:
+            self.transition.mode = 'push'
+            self.transition.direction = 'down'
+            self.current = 'main_screen'
 
 
 class MinesweeperApp(App):
+    is_desktop = Window._is_desktop
+
     def build(self):
+        print(self.is_desktop)
         return MainLayout()
 
 
